25a6ec15c8e4ed3b7bc4fed7e6204cde6b038b56
Sameh96M <Sameh.AbuRadi@CODE.Berlin>
2022-01-18 00:25:35 +0200

    officially adapting peer-to-peer architecture

--- ctrl/view.control.js
+++ ctrl/view.control.js
@@ -2,12 +2,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js');
+}
+
 window.onload = function () {
     document.getElementById('loader').style.display = 'none';
 
     url = new URL(window.location.href);
 
-    // window.sessionStorage.setItem('micAccess', 0);
     window.sessionStorage.setItem('lang', url.searchParams.get('l') ? url.searchParams.get('l') : null);
 
     setTimeout(function () {
@@ -27,49 +30,48 @@ window.onload = function () {
             switchToEnglish();
         }
     }, 50);
+
+    // ---
+
+    // Checking if this is running on a PC
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('entryForm').style.display = 'block';
+        document.getElementById('langBtn').style.display = 'block';
+        document.getElementById('QR.entryForm').src += window.location.href;
+        document.getElementById('QR.PCBrowser').src += window.location.href;
+    } else {
+        document.getElementById('body').style.backgroundColor = '#008230';
+        document.getElementById('PCBrowser').style.display = 'block';
+        document.getElementById('QR.PCBrowser').src += window.location.href;
+        document.getElementById('QR.entryForm').src += window.location.href;
+    }
+
+    // Checking if this is running on iOS
+
+    const isIos = () => {
+        const userAgent = window.navigator.userAgent.toLowerCase();
+
+        window.sessionStorage.setItem('os', 'iOS');
+
+        return /ip(hone|od)/.test(userAgent);
+    };
+
+    // Checking if this is running in standalone mode
+
+    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+    if (isIos() && !isInStandaloneMode()) {
+        setTimeout(function () { document.getElementById('snackbar').classList.add('show'); }, 500);
+    }
+
+    function hide() {
+        setTimeout(function () { document.getElementById('snackbar').classList.remove('show'); }, 50);
+        // setTimeout(function(){document.getElementById('snackbar').classList.add('hide');}, 50);
+    }
+
+    // Checking if this is running on Android
 };
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
-}
-
-// Checking if this is running on a PC
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById('entryForm').style.display = 'block';
-    document.getElementById('QR.entryForm').src += window.location.href;
-    document.getElementById('QR.PCBrowser').src += window.location.href;
-} else {
-    document.getElementById('body').style.backgroundColor = '#008230';
-    document.getElementById('PCBrowser').style.display = 'block';
-    document.getElementById('QR.PCBrowser').src += window.location.href;
-    document.getElementById('QR.entryForm').src += window.location.href;
-}
-
-// Checking if this is running on iOS
-
-const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-
-    window.sessionStorage.setItem('os', 'iOS');
-
-    return /ip(hone|od)/.test(userAgent);
-};
-
-// Checking if this is running in standalone mode
-
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-if (isIos() && !isInStandaloneMode()) {
-    setTimeout(function () { document.getElementById('snackbar').classList.add('show'); }, 500);
-}
-
-function hide() {
-    setTimeout(function () { document.getElementById('snackbar').classList.remove('show'); }, 50);
-    // setTimeout(function(){document.getElementById('snackbar').classList.add('hide');}, 50);
-}
-
-// Checking if this is running on Android
 
 // ---
 
--- sw.js
+++ sw.js
@@ -2,26 +2,60 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-);
+// workbox.routing.registerRoute(({ request }) => request.destination === 'image', new workbox.strategies.CacheFirst());
 
-// self.addEventListener('install', function(event) {
-//     event.waitUntil(
-//         caches.open(cacheName).then(function(cache) {
-//             return cache.addAll(
-//                 [
-//                     '/index.html',
-//                     '/style.css',
-//                     '/ctrl/view.control.js',
-//                     '/ctrl/lang.control.js',
-//                     '/ctrl/form.control.js',
-//                     '/ctrl/join.control.js',
-//                     '/ctrl/chat.control.js',
-//                     '/ctrl/talk.control.js'
-//                 ]
-//             );
-//         })
-//     );
-// });
+const CACHE = 'freeTalk.PS';
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/en.manifest.json',
+                '/web.config',
+                '/style.css',
+                '/sw.js',
+                '/icons/favicon.ico',
+                '/icons/freeTalk.png',
+                '/icons/apple-icon-180.png',
+                '/icons/manifest-icon-192.maskable.png',
+                '/icons/manifest-icon-512.maskable.png',
+                '/ctrl/view.control.js',
+                '/ctrl/lang.control.js',
+                '/ctrl/join.control.js',
+                '/404/offline.html'
+            ]);
+        })
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResponse = await event.preloadResponse;
+
+                if (preloadResponse) return preloadResponse;
+
+                const networkResponse = await fetch(event.request);
+
+                return networkResponse;
+            } catch (e) {
+                const cache = await caches.open(CACHE);
+                const cacheedResponse = await cache.match('/404/offline.html');
+
+                return cacheedResponse;
+            }
+        })());
+    }
+});
